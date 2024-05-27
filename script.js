d93a81898d954f314d1e0e2428a41327b84d2ae9
jghughesai <162188970+jghughesai@users.noreply.github.com>
2024-05-27 15:34:51 -0400

    Setup files

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+const myLibrary = ["LOTR", "Hobbit", "Narnia"];
+
+function Book() {
+    
+}
+
+function addBookToLibrary() {
+
+}
+
+function displayBooks() {
+    myLibrary.forEach((book) => console.log(book));
+}
+
+displayBooks()
