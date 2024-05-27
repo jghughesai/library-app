9a19dd22bdf029bbdf2cf25a2c2d9a1bdc0997d3
jghughesai <162188970+jghughesai@users.noreply.github.com>
2024-05-27 16:24:44 -0400

    Push new book details to 'myLibrary' array

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+const addBtn = document.querySelector("#addBtn");
+const submitBtn = document.querySelector("#submitBtn");
+const dialogBox = document.querySelector("dialog");
 
-
-const myLibrary = ["LOTR", "Hobbit", "Narnia"];
+const myLibrary = [];
 
 function Book() {
     
@@ -14,4 +16,27 @@ function displayBooks() {
     myLibrary.forEach((book) => console.log(book));
 }
 
-displayBooks()
+addBtn.addEventListener("click", () => {
+    console.log("clicked");
+    dialogBox.showModal();
+});
+
+submitBtn.addEventListener("click", (e) => {
+    console.log("submitted")
+    e.preventDefault();
+    dialogBox.close();
+
+    const bookArr = document.querySelectorAll(".details");
+    let result = "";
+
+    bookArr.forEach((input) => result += input.value + ", ");
+    
+    if (result.charAt(result.length -2) === ",") {
+        let index = result.length - 2;
+        result = result.slice(0, index)
+    }
+
+    myLibrary.push(result);
+    console.log(myLibrary);
+
+});
